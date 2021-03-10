49e07cec645172a31fc1e836abd006097939c90c
Ondřej Zahradník <ondrej.zahradnik@avast.com>
2021-03-10 08:33:23 +0100

    update till yesterday data

--- jeockovanibezpecne/stats.py
+++ jeockovanibezpecne/stats.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask import render_template, send_from_directory, request
 from flask import current_app as app
 
-from .models import Submit, Vaccine, Dead, Case
+from .models import Vaccine, Dead, Case
 
 
 bp = Blueprint("stats", __name__)
@@ -17,25 +17,25 @@ def hello():
 
 @bp.route("/", methods=["GET"])
 def index():
-    submit = Submit.query.order_by(Submit.date_for.desc()).first()
-    date_for = submit.date_for
     date_from = datetime(2020, 12, 27)
     cumulative_date_from = datetime(2020, 12, 26)
+
+    cases = Case.query.order_by(Case.date_for.desc()).all()
+    date_for = cases[0].date_for
+
+    cases_from = Case.query.filter(Case.date_for == cumulative_date_from).first()
+    cases_for = Case.query.order_by(Case.date_for.desc()).first()
+    cases_no = cases_for.cases_cumulative - cases_from.cases_cumulative
 
     vaccines = Vaccine.query.filter(Vaccine.date_for >= date_from).filter(Vaccine.date_for <= date_for).all()
     vaccinated_no = sum([v.first_vaccines for v in vaccines])
 
     deads_from = Dead.query.filter(Dead.date_for == cumulative_date_from).first()
-    deads_for = Dead.query.filter(Dead.date_for == date_for).first()
+    deads_for = Dead.query.order_by(Dead.date_for.desc()).first()
     deads_no = deads_for.deads_cumulative - deads_from.deads_cumulative
-
-    cases_from = Case.query.filter(Case.date_for == cumulative_date_from).first()
-    cases_for = Case.query.filter(Case.date_for == date_for).first()
-    cases_no = cases_for.cases_cumulative - cases_from.cases_cumulative
 
     return render_template(
         "index.jinja2",
-        submit=submit,
         vaccinated=vaccinated_no,
         deads=deads_no,
         date_from=date_from,
