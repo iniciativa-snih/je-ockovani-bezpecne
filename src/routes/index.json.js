5c950859472d52a85299aa083ae3f6b03a5ce7f5
Ondřej Zahradník <ondrej.zahradnik@avast.com>
2021-08-26 16:33:29 +0200

    netlify

--- src/routes/index.json.js
+++ src/routes/index.json.js
@@ -3,11 +3,10 @@ import submits from '/data/submits.json';
 import vaccines from '/data/vaccines.json';
 import date from 'date-and-time'
 
-s = stats.pop();
-v = vaccines.pop();
-d = Date.parse(submits.date_for);
-
 export const get = async (request) => {
+    const s = stats[stats.length - 1]
+    const v = vaccines[vaccines.length - 1]
+    const d = Date.parse(submits.date_for)
 
     return {
         body: {
